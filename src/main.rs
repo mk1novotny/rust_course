095409b49f58a36cbddcde6b6308becdac9af3b1
Marek Novotný <novotny@embedix.eu>
2025-01-27 09:20:52 +0100

    lesson_2 HO

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-use std::io;
-use std::io::Write;
+use slug::slugify;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    let mut input = String::new();
-    print!("Type something: ");
-    io::stdout().flush().unwrap();
-    let res = io::stdin().read_line(&mut input);
-    match res {
-        Ok(_) => println!("Read line successfully"),
-        Err(e) => println!("Error reading line: {}", e),
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 3 {
+        println!("Usage: helloworld [lowercase, uppercase, no-spaces, slugify] <input>");
+        return;
     }
-    println!("You typed: {}", input);
-}
+    
+    let input: String = if args.len() > 3 {
+        let mut input: String = "".to_string(); 
+        for i in 2..args.len() {
+            input.push(' ');
+            input.push_str( &args[i]);
+        }
+        input
+    } else {
+        args[2].clone()
+    };
+
+    let result: String = match args[1].as_str() {
+        "lowercase" => {
+            input.to_lowercase()
+        },
+        "uppercase" => {
+            input.to_uppercase()
+
+        },
+        "no-spaces" => {
+            input.replace(" ", "")
+            
+        },
+        "slugify" => {
+            slugify(&input)
+        },
+        _ => {
+            "Unknown command".to_string()
+        }
+    };
+    println!("{}", result);
+}  
